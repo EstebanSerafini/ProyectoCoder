7a8bea02c5ffe86b68907a543f398a35017de66f
Esteban Serafini <estebanserafini@gmail.com>
2023-02-07 19:26:06 -0300

    punto seguro, models listos

---
+++ AppCoder/urls.py
@@ -0,0 +1,10 @@
+from  django.urls import path
+from AppCoder import views
+
+urlpatterns = [
+    path('', views.inicio),
+    path('cursos', views.cursos, name = "cursos"),
+    path('profesores', views.profesores),
+    path('estudiantes', views.estudiantes),
+    path('entregables', views.entregables),
+]
---
+++ AppCoder/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from AppCoder.models import Curso
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
+    #return HttpResponse('vista inicio')
+
+def cursos(request):
+    return render(request,'AppCoder/cursos.html')
+    #return HttpResponse('vista cursos')
+
+def profesores(request):
+    return render(request, 'AppCoder/profesores.html')
+    #return HttpResponse('vista profesores')
+
+def entregables(request):
+    return render(request, 'AppCoder/entregables.html')
+    #return HttpResponse('vista entregables')
+
+def estudiantes(request):
+    return render(request, 'AppCoder/estudiantes.html')
+    #return HttpResponse('vista estudiantes')
