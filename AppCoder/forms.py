a3f4a68ad22ca09919a509df33e2d96a9972cd47
Esteban Serafini <estebanserafini@gmail.com>
2023-02-19 19:14:05 -0300

    Version Preentrega 3 190223

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -2,12 +2,21 @@ from django import forms
 
 
 class CursoFormulario(forms.Form):
-    #Especificar los campos
     curso = forms.CharField()
     camada = forms.IntegerField()
     
 class ProfesorFormulario(forms.Form):
     nombre=forms.CharField(max_length=30)
     apellido=forms.CharField(max_length=30)
-    email = forms.EmailField()
+    email = forms.CharField()
     profesion = forms.CharField(max_length=30)
+
+class EstudianteFormulario(forms.Form):
+    nombre=forms.CharField(max_length=30)
+    apellido=forms.CharField(max_length=30)
+    email = forms.CharField()
+
+class EntregableFormulario(forms.Form):
+    nombre=forms.CharField(max_length=40)
+    fechaDeEntrega = forms.DateField()
+    entregado = forms.BooleanField()
--- AppCoder/models.py
+++ AppCoder/models.py
@@ -12,6 +12,9 @@ class Estudiante(models.Model):
     nombre=models.CharField(max_length=30)
     apellido=models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Email: {self.email}"
     
 class Profesor(models.Model):
     nombre=models.CharField(max_length=30)
@@ -26,3 +29,7 @@ class Entregable(models.Model):
     nombre=models.CharField(max_length=40)
     fechaDeEntrega = models.DateField()
     entregado = models.BooleanField()
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Fecha De Entrega: {self.fechaDeEntrega} - Entregado: {self.entregado}"
+    
